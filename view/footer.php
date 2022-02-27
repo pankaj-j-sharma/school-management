f8bc7f90b9369d6eaa535a770fcc35a9016633a2
Pankaj Sharma <pankajs579@gmail.com>
2022-02-27 23:55:02 +0530

    forked into personal repository

---
+++ view/footer.php
@@ -0,0 +1,9 @@
+	<footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Version</b> 1.0.2
+        </div>
+        <strong>Copyright &copy; 2019-2020 <a href="">Incampus IT Solutions</a>.</strong> All rights reserved.
+     </footer>
+      
+  </body>
+</html>
